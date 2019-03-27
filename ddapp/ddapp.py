1bdc0c2c461871761f52ef31c5cf1b1d4ddf4f6e
David Dansby <ddansby89@gmail.com>
2019-03-27 01:36:55 -0700

    updated requirements.txt; addded small tweaks to notebook; added spark session, saved best pipeline/model, and more flask api items

--- ddapp/ddapp.py
+++ ddapp/ddapp.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restplus import Api, Resource
+from pyspark.ml.feature import OneHotEncoderEstimator, VectorAssembler
+from pyspark.ml.feature import StringIndexer, StandardScaler
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+from pyspark.sql import SparkSession
 
 
 ddapp = Flask(__name__)
@@ -11,9 +17,30 @@ api = Api(app=ddapp,
 lrmod = api.namespace('lrmodel',
                       description='API used to predict total goals of match')
 
+
 @lrmod.route("/")
 class LogReg(Resource):
     def get(self):
         """
         returns a prediction label and odds as JSON object
         """
+        model_inputs = request.get_json
+
+        df = spark.createDataFrame(model_inputs)
+        lr_fit = bestPipe.transform(df)
+
+        probs = lr_fit.select(lr_fit.probability)
+        preds = lr_fit.select(lr_fit.prediction)
+
+        return 'Hello Tester'
+
+
+if __name__ == '__main__':
+    ddapp.run(debug=True)
+
+    spark = SparkSession \
+        .builder \
+        .appName("pysparkDDapp") \
+        .getOrCreate()
+
+    bestPipe = CrossValidator.load('bestLogmodel')
