83015a0795b1882ce92f7de762299276cd37c54b
David Dansby <ddansby89@gmail.com>
2019-03-27 23:51:03 -0700

    added to gitignore; saved best pipeline model since last one was messed up, fixed spark session and pyspark model load issues in flask app, fixed post request so that it can return json

--- ddapp/ddapp.py
+++ ddapp/ddapp.py
@@ -1,8 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restplus import Api, Resource
 from pyspark.ml.feature import OneHotEncoderEstimator, VectorAssembler
 from pyspark.ml.feature import StringIndexer, StandardScaler
-from pyspark.ml import Pipeline
+from pyspark.ml import PipelineModel
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 from pyspark.sql import SparkSession
@@ -18,29 +18,40 @@ lrmod = api.namespace('lrmodel',
                       description='API used to predict total goals of match')
 
 
+spark = SparkSession \
+    .builder \
+    .master('local') \
+    .appName("pysparkDDapp") \
+    .getOrCreate()
+
+bestPipe = PipelineModel.load('bestPipeLogReg')
+
+jsondd = [{"HomeTeam": "Burnley", "AwayTeam": "Huddersfield",
+           "homeLast5goals": 5, "awayLast5goals": 2,
+           "homeLast5shots_on": 10, "awayLast5shots_on": 10},
+          {"HomeTeam": "Man City", "AwayTeam": "Wolves",
+           "homeLast5goals": 10, "awayLast5goals": 10,
+           "homeLast5shots_on": 30, "awayLast5shots_on": 25}]
+
+
 @lrmod.route("/")
 class LogReg(Resource):
-    def get(self):
+    def post(self):
         """
         returns a prediction label and odds as JSON object
         """
-        model_inputs = request.get_json
 
-        df = spark.createDataFrame(model_inputs)
-        lr_fit = bestPipe.transform(df)
+        _model_inputs = request.get_json()
 
-        probs = lr_fit.select(lr_fit.probability)
-        preds = lr_fit.select(lr_fit.prediction)
+        #_df = spark.read.json(_model_inputs, multiLine=True)
 
-        return 'Hello Tester'
+        #lr_fit = bestPipe.transform(_df)
+
+        #probs = lr_fit.select(lr_fit.probability)
+        #preds = lr_fit.select(lr_fit.prediction)
+
+        return jsonify(_model_inputs)
 
 
 if __name__ == '__main__':
     ddapp.run(debug=True)
-
-    spark = SparkSession \
-        .builder \
-        .appName("pysparkDDapp") \
-        .getOrCreate()
-
-    bestPipe = CrossValidator.load('bestLogmodel')
