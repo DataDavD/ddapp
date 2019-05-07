12361be2fb05fff8e3add2e3baf9b5a63390509c
David Dansby <ddansby89@gmail.com>
2019-05-07 00:57:37 -0700

    convert half of emr_spinner to class object with appropriate methods

--- emr_etl/emr_spinner.py
+++ emr_etl/emr_spinner.py
@@ -1,112 +1,137 @@
 import boto3
 from botocore.exceptions import WaiterError
 
-# connect to EMR client
-emr_client = boto3.client('emr', region_name='us-east-1')
 
-# create key for emr ec2 instance just in case need to SSH into cluster
-ec2 = boto3.client('ec2', region_name='us-east-1')
-create_key_response = ec2.create_key_pair(KeyName='ec2_emr_key')
-unkey = str(create_key_response['KeyMaterial'])
-with open('testkey.pem', 'w') as f:
-    f.write(unkey)
+class ClusterFun:
+    """
+    Class used to represent EMR cluster creation, job submission (spark),
+    and spin down.
 
-response = emr_client.run_job_flow(
-    Name="ddapi EMR Cluster4",
-    LogUri='s3://ddapi.data/logs',
-    ReleaseLabel='emr-5.23.0',
-    Instances={
-        'InstanceGroups': [
-            {
-                'Name': "Master nodes",
-                'Market': 'ON_DEMAND',
-                'InstanceRole': 'MASTER',
-                'InstanceType': 'm4.large',
-                'InstanceCount': 1,
+    Attributes
+
+    :param region: region to deploy EMR cluster, use std aws naming convention
+    :type region: str
+    :returns: list of emr client and ec2 client response; used in job flow
+    """
+
+    # Initializer / Instance Attributes
+    def __init__(self,
+                 region='us-east-1',
+                 clustername='ClusterFun EMR Cluster'):
+        self.region = region
+        self.emr_client = boto3.client('emr', region_name=region)
+        # create key for emr ec2 instance just in case need to SSH into cluster
+        self.ec2 = boto3.client('ec2', region_name='us-east-1')
+        self.key = 'emr_key'
+        self.create_key_response = self.ec2.create_key_pair(KeyName=self.key)
+
+    # write key-pair pem file method
+    def storeKey(self, pem_path='emr_keypair.pem'):
+        if not isinstance(pem_path, str):
+            raise TypeError('pem_path must be string or None (default)')
+        else:
+            self.pem_path = pem_path
+            emr_keypem = str(self.create_key_response['KeyMaterial'])
+            with open(pem_path+'emr_keypair.pem', 'w') as f:
+                f.write(emr_keypem)
+
+    # cluster spin up method
+    def spinUp(self):
+        response = self.emr_client.run_job_flow(
+            Name="ddapi EMR Cluster",
+            LogUri='s3://ddapi.data/logs',
+            ReleaseLabel='emr-5.23.0',
+            Instances={
+                'InstanceGroups': [
+                    {
+                        'Name': "Master nodes",
+                        'Market': 'ON_DEMAND',
+                        'InstanceRole': 'MASTER',
+                        'InstanceType': 'm4.large',
+                        'InstanceCount': 1,
+                    },
+                    {
+                        'Name': "Slave nodes",
+                        'Market': 'ON_DEMAND',
+                        'InstanceRole': 'CORE',
+                        'InstanceType': 'm4.large',
+                        'InstanceCount': 2,
+                    }
+                ],
+                'Ec2KeyName': self.key,
+                'KeepJobFlowAliveWhenNoSteps': True,
+                'TerminationProtected': False,
+                # 'Ec2SubnetId': 'string',
             },
-            {
-                'Name': "Slave nodes",
-                'Market': 'ON_DEMAND',
-                'InstanceRole': 'CORE',
-                'InstanceType': 'm4.large',
-                'InstanceCount': 2,
-            }
-        ],
-        'Ec2KeyName': 'ec2_emr_key',
-        'KeepJobFlowAliveWhenNoSteps': True,
-        'TerminationProtected': False,
-        # 'Ec2SubnetId': 'string',
-    },
-    Applications=[
-        {'Name': 'Hadoop'},
-        {'Name': 'Spark'}
-    ],
-    BootstrapActions=[
-        {
-            'Name': 'bootstrap requirements',
-            'ScriptBootstrapAction': {
-                'Path': 's3://ddapi.data/ddapp_emr_bootstrap.sh',
-            }
-        },
-    ],
-    VisibleToAllUsers=True,
-    JobFlowRole='EMR_EC2_DefaultRole',
-    ServiceRole='EMR_DefaultRole',
-    Configurations=[
-        {
-            "Classification": "spark-env",
-            "Configurations": [
+            Applications=[
+                {'Name': 'Hadoop'},
+                {'Name': 'Spark'}
+            ],
+            BootstrapActions=[
                 {
-                    "Classification": "export",
+                    'Name': 'bootstrap requirements',
+                    'ScriptBootstrapAction': {
+                        'Path': 's3://ddapi.data/ddapp_emr_bootstrap.sh',
+                        }
+                },
+                ],
+            VisibleToAllUsers=True,
+            JobFlowRole='EMR_EC2_DefaultRole',
+            ServiceRole='EMR_DefaultRole',
+            Configurations=[
+                {
+                    "Classification": "spark-env",
+                    "Configurations": [
+                        {
+                            "Classification": "export",
+                            "Properties": {
+                                "PYSPARK_PYTHON": "/usr/bin/python3",
+                                "PYSPARK_DRIVER_PYTHON": "/usr/bin/python3"
+                                }
+                        }
+                        ]
+                },
+                {
+                    "Classification": "spark-defaults",
                     "Properties": {
-                        "PYSPARK_PYTHON": "/usr/bin/python3",
-                        "PYSPARK_DRIVER_PYTHON": "/usr/bin/python3"
+                        "spark.sql.execution.arrow.enabled": "true"
+                        }
+                },
+                {
+                    "Classification": "spark",
+                    "Properties": {
+                        "maximizeResourceAllocation": "true"
                         }
                 }
-            ]
-        },
-        {
-            "Classification": "spark-defaults",
-            "Properties": {
-                "spark.sql.execution.arrow.enabled": "true"
-                }
-        },
-        {
-            "Classification": "spark",
-            "Properties": {
-                "maximizeResourceAllocation": "true"
-                }
-        }
-    ],
-)
+            ],
+            )
 
-job_flow_id = response['JobFlowId']
-print("Job", job_flow_id, "is running")
+        self.job_flow_id = response['JobFlowId']
 
-# get cluster id
-resp = emr_client.list_clusters()
-clus = resp['Clusters'][0]
-clusID = clus['Id']
+        # get cluster id
+        resp = self.emr_client.list_clusters()
+        clus = resp['Clusters'][0]
+        self.clusID = clus['Id']
 
-create_waiter = emr_client.get_waiter('cluster_running')
-try:
-    create_waiter.wait(ClusterId=clusID,
-                       WaiterConfig={
-                           'Delay': 15,
-                           'MaxAttempts': 120
-                       })
+        create_waiter = self.emr_client.get_waiter('cluster_running')
+        try:
+            create_waiter.wait(ClusterId=self.clusID,
+                               WaiterConfig={
+                                   'Delay': 15,
+                                   'MaxAttempts': 120
+                                   })
 
-except WaiterError as e:
-    if 'Max attempts exceeded' in e.message:
-        print('EMR Step did not complete in 30 minutes')
-    else:
-        print(e.message)
+        except WaiterError as e:
+            if 'Max attempts exceeded' in e.message:
+                print('EMR Step did not complete in 30 minutes')
+            else:
+                print(e.message)
 
 # don't forget to tip the waiter :)
 
 
 step_response = emr_client.add_job_flow_steps(
-    JobFlowId=job_flow_id,
+    JobFlowId=self.job_flow_id,
     Steps=[
         {
             'Name': 'setup - copy emr test py file',
@@ -137,7 +162,7 @@ print("Step IDs Running:", steps_id)
 
 step_waiter = emr_client.get_waiter('step_complete')
 try:
-    step_waiter.wait(ClusterId=clusID,
+    step_waiter.wait(ClusterId=self.clusID,
                      StepId=steps_id[1],
                      WaiterConfig={
                          'Delay': 15,
@@ -152,13 +177,13 @@ except WaiterError as e:
 
 # don't forget to tip the waiter :)
 
-response = emr_client.terminate_job_flows(
-    JobFlowIds=[job_flow_id]
+response = self.emr_client.terminate_job_flows(
+    JobFlowIds=[self.job_flow_id]
     )
 
-spinDown_waiter = emr_client.get_waiter('cluster_terminated')
+spinDown_waiter = self.emr_client.get_waiter('cluster_terminated')
 try:
-    spinDown_waiter.wait(ClusterId=clusID)
+    spinDown_waiter.wait(ClusterId=self.clusID)
 
 except WaiterError as e:
     if 'Max attempts exceeded' in e.message:
@@ -169,4 +194,4 @@ except WaiterError as e:
 # don't forget to tip the waiter :)
 
 # delete key after job run
-key_del_response = ec2.delete_key_pair(KeyName='ec2_emr_key')
+key_del_response = ec2.delete_key_pair(KeyName=self.key)
