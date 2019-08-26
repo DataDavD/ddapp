c3f9ed8de6bf3e1b0b9f28843da5796bed27d55a
David Dansby <ddansby89@gmail.com>
2019-08-25 21:16:53 -0700

    - updt bootstrap bash

--- emr_etl/ddapp_emr_bootstrap.sh
+++ emr_etl/ddapp_emr_bootstrap.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
 # installs deps
-sudo yum -y update # just in case
+sudo yum -y update
 sudo python3 -m pip install --upgrade pip
-sudo yum -y update # just in case
-sudo python3 -m pip install awscli pandas requests
-sudo yum -y update # just in case
-
-# add the bash commands below if need to pip install from requirements.txt
-# sudo aws s3 cp s3://ddapi.data/requirements.txt requirements.txt
-# sudo python3 -m pip install -r requirements.txt
+sudo yum -y update
+sudo python3 -m pip install awscli pandas requests pyarrow numpy pyspark
+sudo yum -y update
